099c60147f0774aeef66c6d920d653f36f8cd43c
cody8522 <64490876+cody8522@users.noreply.github.com>
2022-12-06 13:06:01 -0600

    Update stagefront.py

--- app/stagefront.py
+++ app/stagefront.py
@@ -73,91 +73,95 @@ if submit_button:
     st.header(Artist)
     st.write(Venue_City, ",", Venue_State)
     st.write(Event_Datetime)
-
-
-    #Sales Information
-    sales_df = pd.json_normalize(sales)
-
-    sales_df2 = sales_df[['putc', 'bp', 'q' ,'s', 'r']]
-
-    df = sales_df2['putc'].str.split("-",2, expand=True)
-
-    df2 = df[2].str.split("T", 1, expand = True)
-
-
-    sales_df2['Year'] = df[0]
-    sales_df2['Month'] = df[1]
-    sales_df2['Day'] = df2[0]
-    sales_df2['Time'] = df2[1]
-    sales_df2.drop(columns = ["putc"], inplace = True)
-
-    df_rename = sales_df2.rename(columns = {'bp':'Price', 'q' : 'Qty', 's' : 'Sec', 'r' : 'Row'})
-
-    final_df = df_rename[['Year', 'Month', 'Day', 'Time', 'Price', 'Qty', 'Sec', 'Row']]
-    final_df = final_df.sort_values(by=['Year', 'Month','Day', 'Time'])
-
-
-    final_df['date_string'] = final_df['Year'].astype(str) + "-" + final_df['Month'].astype(str)+ "-" +  final_df['Day'].astype(str)    
-
-    ##volume chart
-    v_scale = [0, max(final_df['Qty']) + 5]
-    volume_chart = alt.Chart(final_df).mark_bar().encode(
-    x = alt.X('date_string', axis = alt.Axis(labelOverlap = True, labelSeparation = 10), title = 'Date'),
-    y =alt.Y('Qty', scale =alt.Scale(domain = v_scale)),
-        tooltip = ['date_string', 'Qty'],).interactive().properties(width = 500)
-
-
-    ##price chart
-    x_scale = [min(final_df['Price']) - 20, max(final_df['Price']) + 20]
-    price_chart = alt.Chart(final_df).mark_line().encode(
-    x = alt.X('date_string', axis = alt.Axis(labelOverlap = True, labelSeparation = 10), scale=alt.Scale(zero=False), title = 'Date'),
-    y = alt.Y('mean(Price)', scale = alt.Scale(domain = x_scale), title = 'Price'),
-    tooltip = ['date_string','mean(Price)'],).interactive().properties(width = 500)
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.header("Price over Time")
-        st.altair_chart(price_chart, use_container_width=True)
-    with col2:
-        st.header("Volume over Time")
-        st.altair_chart(volume_chart, use_container_width=True)
     
-    #AgGrid Chart
-    st.subheader("Sales History")
-    AgGrid(final_df.sort_values(by = ['Year','Month','Day','Time'],
-     ascending = False),
-     fit_columns_on_grid_load = True, theme = 'dark')
-    
-    #Download CSV
-    final_df = final_df.to_csv(index = False).encode('utf-8')
-    st.download_button(label = "Export to CSV", data = final_df, 
-        file_name ="seatgeek_sales.csv", mime = 'text/csv')
-    
-    #SeatGeek API for Current Stats
-    api_url_full = "https://api.seatgeek.com/2/events/{}?client_id=ODA3MjA1NXwxNjU2NzA1Njk2LjYyNTE0Mzg&client_secret=e81628dfe5fa78b2541818724a291649cb0454a30ea813b8d3d5f1dbdd3c1215"
-    api_url = api_url_full.format(sg_id)
-    api_payload={}
-    api_headers = {
-    'Cookie': '_pxhd=L8w7yqOP6Cx42r4k1-HX8OCd0j0eYOus48gXvrqwdXDfXcS6bNi/nZ0Nfg-FkRaGje5iOiYTdo1Oql9/HE1H2Q==:OsAKtGsLntFg0Z3hsIj26yisUL/rm/ajy0xx7amwXDqOpQ5MTU6HbSupHSdh2ouDwxMu4PI-OB4MZCLbiDYqL-k9WjRIJ99mTHZXX2NOqps=',
-    }
+    if len(sales) == 0:
+        st.write("There have been no sales for this event")
 
-    api_response = requests.request("GET", api_url, headers=api_headers, data=api_payload)
-    api_data = api_response.json()
-    api_data_2 = api_data['stats']
+    if len(sales) != 0:
 
-    test1 = api_data_2.get('listing_count')
+        #Sales Information
+        sales_df = pd.json_normalize(sales)
 
-    current_listings = str(list(api_data_2.values())[0]) 
-    average_price = str(list(api_data_2.values())[1])
-    lowest_price = str(list(api_data_2.values())[3])
-    highest_price = str(list(api_data_2.values())[4])
-    median_price = str(list(api_data_2.values())[7])
+        sales_df2 = sales_df[['putc', 'bp', 'q' ,'s', 'r']]
 
-    st.header("Current Values")
-    st.subheader("Number of Listings:" + " " + current_listings)
-    st.subheader("Get-In Price:" + " " +  "$" + lowest_price)
-    st.subheader("Average Price:" + " " + "$" +average_price)
-    st.subheader("Median Price:" + " " + "$" + median_price)
-    st.subheader("Highest Price:" + " " + "$" +  highest_price)
+        df = sales_df2['putc'].str.split("-",2, expand=True)
+
+        df2 = df[2].str.split("T", 1, expand = True)
+
+
+        sales_df2['Year'] = df[0]
+        sales_df2['Month'] = df[1]
+        sales_df2['Day'] = df2[0]
+        sales_df2['Time'] = df2[1]
+        sales_df2.drop(columns = ["putc"], inplace = True)
+
+        df_rename = sales_df2.rename(columns = {'bp':'Price', 'q' : 'Qty', 's' : 'Sec', 'r' : 'Row'})
+
+        final_df = df_rename[['Year', 'Month', 'Day', 'Time', 'Price', 'Qty', 'Sec', 'Row']]
+        final_df = final_df.sort_values(by=['Year', 'Month','Day', 'Time'])
+
+
+        final_df['date_string'] = final_df['Year'].astype(str) + "-" + final_df['Month'].astype(str)+ "-" +  final_df['Day'].astype(str)    
+
+        ##volume chart
+        v_scale = [0, max(final_df['Qty']) + 5]
+        volume_chart = alt.Chart(final_df).mark_bar().encode(
+        x = alt.X('date_string', axis = alt.Axis(labelOverlap = True, labelSeparation = 10), title = 'Date'),
+        y =alt.Y('Qty', scale =alt.Scale(domain = v_scale)),
+            tooltip = ['date_string', 'Qty'],).interactive().properties(width = 500)
+
+
+        ##price chart
+        x_scale = [min(final_df['Price']) - 20, max(final_df['Price']) + 20]
+        price_chart = alt.Chart(final_df).mark_line().encode(
+        x = alt.X('date_string', axis = alt.Axis(labelOverlap = True, labelSeparation = 10), scale=alt.Scale(zero=False), title = 'Date'),
+        y = alt.Y('mean(Price)', scale = alt.Scale(domain = x_scale), title = 'Price'),
+        tooltip = ['date_string','mean(Price)'],).interactive().properties(width = 500)
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header("Price over Time")
+            st.altair_chart(price_chart, use_container_width=True)
+        with col2:
+            st.header("Volume over Time")
+            st.altair_chart(volume_chart, use_container_width=True)
+
+        #AgGrid Chart
+        st.subheader("Sales History")
+        AgGrid(final_df.sort_values(by = ['Year','Month','Day','Time'],
+         ascending = False),
+         fit_columns_on_grid_load = True, theme = 'dark')
+
+        #Download CSV
+        final_df = final_df.to_csv(index = False).encode('utf-8')
+        st.download_button(label = "Export to CSV", data = final_df, 
+            file_name ="seatgeek_sales.csv", mime = 'text/csv')
+
+        #SeatGeek API for Current Stats
+        api_url_full = "https://api.seatgeek.com/2/events/{}?client_id=ODA3MjA1NXwxNjU2NzA1Njk2LjYyNTE0Mzg&client_secret=e81628dfe5fa78b2541818724a291649cb0454a30ea813b8d3d5f1dbdd3c1215"
+        api_url = api_url_full.format(sg_id)
+        api_payload={}
+        api_headers = {
+        'Cookie': '_pxhd=L8w7yqOP6Cx42r4k1-HX8OCd0j0eYOus48gXvrqwdXDfXcS6bNi/nZ0Nfg-FkRaGje5iOiYTdo1Oql9/HE1H2Q==:OsAKtGsLntFg0Z3hsIj26yisUL/rm/ajy0xx7amwXDqOpQ5MTU6HbSupHSdh2ouDwxMu4PI-OB4MZCLbiDYqL-k9WjRIJ99mTHZXX2NOqps=',
+        }
+
+        api_response = requests.request("GET", api_url, headers=api_headers, data=api_payload)
+        api_data = api_response.json()
+        api_data_2 = api_data['stats']
+
+        test1 = api_data_2.get('listing_count')
+
+        current_listings = str(list(api_data_2.values())[0]) 
+        average_price = str(list(api_data_2.values())[1])
+        lowest_price = str(list(api_data_2.values())[3])
+        highest_price = str(list(api_data_2.values())[4])
+        median_price = str(list(api_data_2.values())[7])
+
+        st.header("Current Values")
+        st.subheader("Number of Listings:" + " " + current_listings)
+        st.subheader("Get-In Price:" + " " +  "$" + lowest_price)
+        st.subheader("Average Price:" + " " + "$" +average_price)
+        st.subheader("Median Price:" + " " + "$" + median_price)
+        st.subheader("Highest Price:" + " " + "$" +  highest_price)
     
